a955a930321fc1c7ea00d56f534064556a6dff44
Zeina <zeinaayman666@gmail.com>
2023-10-07 22:55:08 +0300

    spelling mistake fix

--- src/Models/user.js
+++ src/Models/user.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
     },
     type: {
       type: String,
-      enum: ["adminstrator", "doctor", "patient"],
+      enum: ["administrator", "doctor", "patient"],
       required: true,
     },
   },
