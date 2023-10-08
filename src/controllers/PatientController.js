00b9d1ebb3e0036cebbcbfc1f3e5ebfad8f7a28b
ahmedhemeida98 <ahmedhemeida98@gmail.com>
2023-10-08 20:26:23 +0300

    added view familymember

--- src/controllers/PatientController.js
+++ src/controllers/PatientController.js
@@ -14,7 +14,18 @@ const addFamilyMember = async(req, res) => {
 }
 
 const viewFamilyMembers = async(req, res) => {
-
+    try{
+        const user = await user.findOne({ username }).populate('patient');
+    
+        if (user && user.patient) {
+          const familyMembers = user.patient.family_members;
+          return familyMembers;
+        } else {
+          throw new Error('Patient not found');
+        }
+      } catch (error) {
+        throw new Error(`Error viewing family members: ${error.message}`);
+      }
 }
 
 const viewDoctors = async(req, res) => {
