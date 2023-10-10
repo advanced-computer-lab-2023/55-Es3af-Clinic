96e4b20e9a97affdec510a55859c0c1e796ccf28
sara_a.wasfy@hotmail.com <sara_a.wasfy@hotmail.com>
2023-10-10 19:25:31 +0300

    testfiles

--- src/test.js
+++ src/test.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || "8000";
 const patient = require('./Models/Patient');
 const familyMember = require('./Models/FamilyMembers');
 const patientController = require('./controllers/PatientController')
-const registerPatientController = require('./controllers/RegisterPatientController')
+const registerPatientController = require('./controllers/RegisterPatientController');
+const requestDoctorController = require('./controllers/RequestDoctorController');
 
 
 mongoose.connect(MongoURI, {dbName: 'Clinic'})
@@ -28,4 +29,5 @@ app.use(express.json())
 app.post("/addFamilyMember", patientController.addFamilyMember);
 app.get("/viewFamilyMembers", patientController.viewFamilyMembers)
 app.get("/viewDoctors", patientController.viewDoctors)
-app.post("/requestPatient", registerPatientController.registerPatient )
+app.post("/registerPatient", registerPatientController.registerPatient)
+app.post("/requestDoctor", requestDoctorController.requestDoctor)
