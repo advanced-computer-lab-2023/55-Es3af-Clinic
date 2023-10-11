d5790d02a013c6be09ec8a06c40977c3f9af22df
mohamedsalahh7 <102487011+mohamedsalahh7@users.noreply.github.com>
2023-10-11 13:20:17 +0300

    appointments folder

---
+++ src/Models/Appointments.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const patientModel = require('./Patient.js');
+const doctorModel = require('Doctor.js');
+// const userModel = require('./user.js')
+
+const appointmentsSchema = new Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'patient',
+        autoRemove: true,
+      },
+      doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'doctor',
+        autoRemove: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        default: 30,
+      }
+})
+const appointments= mongoose.model("appointments", appointmentsSchema);
+module.exports = appointments;
