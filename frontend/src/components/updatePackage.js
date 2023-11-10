545e5f1c56039165649a234a0e8f3a50b62d0aed
batsiouny <seifeldin51@gmail.com>
2023-11-11 01:12:53 +0200

    updatePackage updates, still not functional

--- frontend/src/components/updatePackage.js
+++ frontend/src/components/updatePackage.js
@@ -2,58 +2,63 @@ import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import packageService from "../services/packageService";
-import adminService from "../services/adminService";
-
 
 function UpdatePackage() {
-  const initialUserState = {
+  const initialPackageState = {
     type: "",
     price: 0,
     sessionDiscount: 0,
-    medicationDiscount: 0, 
-    familyMemberDiscount: 0
-
   };
 
-  const [pack, setPack] = useState(initialUserState);
+  const [pack, setPack] = useState(initialPackageState);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    {
-      setPack({ ...pack, [name]: value});
-    }
-  }
-  async function update(e) {
+    setPack({ ...pack, [name]: value });
+  };
+
+  const update = async (e) => {
     e.preventDefault();
-    // no need to console log response data, only for testing
-    packageService.UpdatePackage(pack)
-      .then((response) => {
-        //console.log(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
+
+    try {
+      // Send only the relevant data for package update
+      const { type, price, sessionDiscount } = pack;
+
+      // Update the request payload to match the backend expectations
+      const response = await packageService.updatePackage({
+        type,
+        price,
+        sessionDiscount,
       });
-  }
+      
+      console.log(response.data);
+
+      // Optionally, you can reset the form or perform any other actions
+      setPack(initialPackageState);
+    } catch (error) {
+      console.error("Error updating package:", error);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <form className="App-header" onSubmit={update}>
           <div className="form-group">
-            <label htmlFor="InputUsername">package name</label>
+            <label htmlFor="type">Package type</label>
             <input
-              type="string"
+              type="text"
               className="form-control"
               id="type"
               name="type"
               value={pack.type}
-              placeholder="Enter username"
+              placeholder="Enter Package type"
               onChange={handleInputChange}
-            ></input>
+            />
           </div>
 
           <div className="form-group">
-            <label htmlFor="InputPassword1">package price</label>
+            <label htmlFor="price">Package price</label>
             <input
               type="number"
               className="form-control"
@@ -62,22 +67,24 @@ function UpdatePackage() {
               value={pack.price}
               placeholder="Price"
               onChange={handleInputChange}
-            ></input>
+            />
           </div>
+
           <div className="form-group">
-            <label htmlFor="InputPassword1"> sessionDiscount</label>
+            <label htmlFor="sessionDiscount">Session Discount</label>
             <input
-              type="string"
+              type="number"
               className="form-control"
               id="sessionDiscount"
               name="sessionDiscount"
               value={pack.sessionDiscount}
-              placeholder="ingredients"
+              placeholder="Session Discount"
               onChange={handleInputChange}
-            ></input>
+            />
           </div>
+
           <button type="submit" className="btn btn-primary">
-            update package
+            Update Package
           </button>
         </form>
       </header>
