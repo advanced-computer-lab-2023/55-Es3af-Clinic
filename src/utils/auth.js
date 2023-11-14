6231d34fd7e58482a57210c0e716c3a96b480d2c
mohamedsalahh7 <102487011+mohamedsalahh7@users.noreply.github.com>
2023-11-14 12:19:59 +0200

    avduvtdvq

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,14 +4,12 @@ const User = require("../Models/user.js");
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  // check json web token exists & is verified
   if (token) {
     jwt.verify(token, "supersecret", (err, decodedToken) => {
       if (err) {
         console.log("Error in JWT verification:", err);
         res.status(401).json({ message: "You are not logged in." });
       } else {
-        console.log("Decoded Token:", decodedToken);
         next();
       }
     });
