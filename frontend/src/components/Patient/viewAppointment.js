17a700eb58efdb74bd254201fac9a899bda02e1a
farahdawod <farahdawod@outlook.com>
2023-12-10 10:31:32 +0200

    loading

--- frontend/src/components/Patient/viewAppointment.js
+++ frontend/src/components/Patient/viewAppointment.js
@@ -117,8 +117,19 @@ const ViewAppointments = () => {
   return (
     <div>
       {loading ? (
-        <div className="loading-icon">Loading...</div>
-      ) : (
+        <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
+
+                <div class="indicator"> 
+                    <svg width="16px" height="12px">
+                        <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                        <polyline id="front" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>      ) : (
         <div className="App-header">
           <div className="tabs">
         <button
